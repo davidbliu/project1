06fec6d9cbe6c989838ba06a066e709a3def8578
David <davidbliu@gmail.com>
2013-07-02 18:09:04 -0700

    matched google images to layout

--- google images/googleSearch.js
+++ google images/googleSearch.js
@@ -1,48 +1,121 @@
 google.load('search', '1');
-
+var searchTerm;
+var page=0;
+var tableRows=3;
+var tableCols=7;
+var numAdded=0;
+var row=0;
+var col=0;
 var imageSearch;
+var size=80;
+function createButtons(){
+  var div=document.createElement('div');
+  div.className='buttonDiv';
+  for(var i=1;i<6;i++){
+    var b=document.createElement('button');
+    b.className='rateButton';
+    b.innerHTML=i;
+    div.appendChild(b);
+  }
+  return div;
+}
 function createTable(){
   var table=document.getElementById('table');
-  for(var i=0;i<5;i++){
+  for(var i=0;i<tableRows;i++){
     var row=table.insertRow(0);
-    for(var j=0;j<7;j++){
+    for(var j=0;j<tableCols;j++){
       var cell=row.insertCell(j);
-      cell.height=100;
-      cell.width=100;
-      cell.innerHTML="blank";
+      cell.height=size;
+      cell.width=size;
+      cell.className='imageContainer';
     }
   }
 }
 function searchComplete() {
   if (imageSearch.results && imageSearch.results.length > 0) {
-    var contentDiv = document.getElementById('content');
     var results = imageSearch.results;
+    var table=document.getElementById('table');
     for (var i = 0; i < results.length; i++) {
       var result = results[i];
-      var imgContainer = document.createElement('div');
       var newImg = document.createElement('img');
-      newImg.src=result.url;
-      newImg.height=100;
-      newImg.width=100;
-      imgContainer.appendChild(newImg);
-      contentDiv.appendChild(imgContainer);
+      newImg.src=result.unescapedUrl;
+      newImg.height=size;
+      newImg.width=size;
+      if(numAdded<tableCols*tableRows){
+        addToTable(row, col, newImg);
+        numAdded++;
+        col=col+1;
+        if(col>=tableCols){
+          col=0;
+          row=row+1;
+        }
+      }
+      
+    }
+    if(row < tableRows){
+      page+=1;
+      imageSearch.gotoPage(page);
     }
   }
 }
-
-function OnLoad(a) {
+function addToTable(r, c, image){
+  var table=document.getElementById('table');
+  var rows=table.getElementsByTagName('tr');
+  var row=rows[r];
+  var cols=row.getElementsByTagName('td');
+  var cell=cols[c];
+  cell.innerHTML="";
+  cell.appendChild(image);
+}
+function OnLoad(term) {
   imageSearch = new google.search.ImageSearch();
+  imageSearch.setResultSetSize(8);
   imageSearch.setSearchCompleteCallback(this, searchComplete, null);
-  imageSearch.execute(a);
+  imageSearch.execute(term);
 }
 
 google.setOnLoadCallback(function(){
   createTable();
-  $('button').click(function(){
-     OnLoad($('input').val());
+
+  $('#search').click(function(){
+    col=0;
+    row=0;
+    numAdded=0;
+    searchTerm=$('input').val();
+     OnLoad(searchTerm);
      $('input').text("");
-    $('#count').text(str);
   });
-});
+  $('.test').click(function(){
+    alert();
+  });//end of the testdiv thing
+  $('.imageContainer').hover(
+    function(){
+      $(this).stop(true,true).animate({
+        height:'+=50px',
+        width:'+=100px'
+        // $('#buttons').clone().appendTo(this);
+      });
+      var buttons=createButtons();
+      buttons.id="asdf"
+      $(this).append(buttons);
+    },
+    function(){
+      $(this).stop(true,true).height(size);
+      $(this).stop(true,true).width(size);
+      $('#asdf').remove();
+    });
+  $('li').hover(
+    function(){ //mouse over
+      // $(this).css("background_color", "white");
+      $(this).css('background-color', 'white');
+    },
+    function(){ //mouse away
+      // $(this).css("background_color", "yellow");
+      $(this).css('background-color', 'CCCCCC');
+    }
+    );
+  
+
+}); //end of the google thing
 
       
