528eb833843c835a93fe164ac3830091831fff5c
David <davidbliu@gmail.com>
2013-07-02 18:20:16 -0700

    matched google images to layout

--- google images/googleSearch.js
+++ google images/googleSearch.js
@@ -1,5 +1,4 @@
 google.load('search', '1');
-<<<<<<< HEAD
 var searchTerm;
 var page=0;
 var tableRows=3;
@@ -29,25 +28,11 @@ function createTable(){
       cell.height=size;
       cell.width=size;
       cell.className='imageContainer';
-=======
-
-var imageSearch;
-function createTable(){
-  var table=document.getElementById('table');
-  for(var i=0;i<5;i++){
-    var row=table.insertRow(0);
-    for(var j=0;j<7;j++){
-      var cell=row.insertCell(j);
-      cell.height=100;
-      cell.width=100;
-      cell.innerHTML="blank";
->>>>>>> 153b7a5afd833f10d501235c03ccc98730f91d89
     }
   }
 }
 function searchComplete() {
   if (imageSearch.results && imageSearch.results.length > 0) {
-<<<<<<< HEAD
     var results = imageSearch.results;
     var table=document.getElementById('table');
     for (var i = 0; i < results.length; i++) {
@@ -87,32 +72,10 @@ function OnLoad(term) {
   imageSearch.setResultSetSize(8);
   imageSearch.setSearchCompleteCallback(this, searchComplete, null);
   imageSearch.execute(term);
-=======
-    var contentDiv = document.getElementById('content');
-    var results = imageSearch.results;
-    for (var i = 0; i < results.length; i++) {
-      var result = results[i];
-      var imgContainer = document.createElement('div');
-      var newImg = document.createElement('img');
-      newImg.src=result.url;
-      newImg.height=100;
-      newImg.width=100;
-      imgContainer.appendChild(newImg);
-      contentDiv.appendChild(imgContainer);
-    }
-  }
-}
-
-function OnLoad(a) {
-  imageSearch = new google.search.ImageSearch();
-  imageSearch.setSearchCompleteCallback(this, searchComplete, null);
-  imageSearch.execute(a);
->>>>>>> 153b7a5afd833f10d501235c03ccc98730f91d89
 }
 
 google.setOnLoadCallback(function(){
   createTable();
-<<<<<<< HEAD
 
   $('#search').click(function(){
     col=0;
@@ -154,13 +117,5 @@ google.setOnLoadCallback(function(){
   
 
 }); //end of the google thing
-=======
-  $('button').click(function(){
-     OnLoad($('input').val());
-     $('input').text("");
-    $('#count').text(str);
-  });
-});
->>>>>>> 153b7a5afd833f10d501235c03ccc98730f91d89
 
       
